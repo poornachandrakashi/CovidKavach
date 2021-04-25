e252c88931f26984ac4c60cebca60bec66ea8b67
poornachandrakashi <poornachandra.kashi@gmail.com>
2021-04-25 12:05:58 +0530

    Raghav updated

--- app.py
+++ app.py
@@ -51,33 +51,35 @@ def fetch_vaccine():
     # return final[0]
     return render_template("vacc.html",final = final)
 
-# @app.route('/fetch_beds')
-# def fetch_oxy():
-#     tweets = tweepy.Cursor(api.search,
-#               q=search_words,
-#               lang="en",
-#               since=date_since).items(15)
-#     data = []
-#     for tweet in tweets:
-#         data.append(tweet.text)
+@app.route('/fetch_beds')
+def fetch_beds():
+    tweets = tweepy.Cursor(api.search,
+              q="hospital beds",
+              lang="en",
+              since=date_since).items(15)
+    data = []
+    for tweet in tweets:
+        data.append(tweet.text)
 
-#     final = data
-#     # return final[0]
-#     return render_template("oxy.html",final = final)
+    final = data
+    # return final[0]
+    return render_template("beds.html",final = final)
 
-# @app.route('/fetch_remdiviser')
-# def fetch_oxy():
-#     tweets = tweepy.Cursor(api.search,
-#               q=search_words,
-#               lang="en",
-#               since=date_since).items(15)
-#     data = []
-#     for tweet in tweets:
-#         data.append(tweet.text)
+# @app.route('/fetch_pm')
+# def f
+# etch_pm():
+#     number_of_tweets = 100
+#     tweets = []
+#     likes = []
+#     time = []
 
-#     final = data
-#     # return final[0]
-#     return render_template("oxy.html",final = final)
+#     for i in tweepy.Cursor(api.user_timeline,id='narendramodi',tweet_mode="extended").items(number_of_tweets):
+#         tweets.append(i.full_text)
+#         likes.append(i.favorite_count)
+#         time.append(i.created_at)
+#         df = pd.DataFrame({'tweets':tweets,'likes':likes,'time':time})
+
+#     return render_template("pm.html", df = df)
 
 # @app.route('/fetch_bedsBangalore')
 # def fetch_oxy():
